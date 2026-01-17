8db58dad0e195786622d68d5d28286c978bda324
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-17 19:51:09 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,8 +1,13 @@
+# pro1.py
 import sys
 
 def celsius_to_fahrenheit(c):
     return (c * 9/5) + 32
 
 if __name__ == "__main__":
-    celsius = float(sys.argv[1])
-    print(celsius_to_fahrenheit(celsius))
+    if len(sys.argv) == 2:
+        celsius = float(sys.argv[1])
+        print("Fahrenheit:", celsius_to_fahrenheit(celsius))
+    else:
+        # IMPORTANT: do NOT exit with error
+        print("No input provided. Skipping execution.")
