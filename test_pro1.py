2d327228f2facb3db8e28acf71e246edb6ccedf9
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-17 19:48:42 +0530

    Update test_pro1.py

--- test_pro1.py
+++ test_pro1.py
@@ -1,10 +1,4 @@
 from pro1 import celsius_to_fahrenheit
 
-def test_zero_celsius():
+def test_zero():
     assert celsius_to_fahrenheit(0) == 32
-
-def test_boiling_point():
-    assert celsius_to_fahrenheit(100) == 212
-
-def test_negative_value():
-    assert celsius_to_fahrenheit(-40) == -40
