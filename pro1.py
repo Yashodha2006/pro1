a56ce6544b6b8e83e1f3f2106300cc016dd1af45
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-18 09:10:58 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -2,9 +2,13 @@ import sys
 
 def celsius_to_fahrenheit(c):
     return (c * 9/5) + 32
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python pro1.py <celsius>")
-        sys.exit(0)
-    celsius = float(sys.argv[1])
-    print("Fahrenheit:", celsius_to_fahrenheit(celsius))
+        print("Please enter Celsius temperature as a command-line argument.")
+        print("Example:")
+        print("  python pro1.py 25")
+        sys.exit(0)  
+
+    c = float(sys.argv[1])
+    print("Fahrenheit:", celsius_to_fahrenheit(c))
